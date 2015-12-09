daa7367720c54c1bca43ccb3ae67431c0a83c22f
Kai Aldag <kaialdag@Kais-MacBook-Pro.local>
2015-12-08 21:29:37 -0800

    working on title translate tests

---
+++ tests/titletranslate_tests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8  -*-
+"""TitleTranslate test module."""
+#
+# (C) Pywikibot team, 2007-2015
+#
+# Distributed under the terms of the MIT license.
+#
+
+from piwikibot import translate
+
+import unittest
+
+class TestTitleTranslate(unittest.TestCase):
+    """ Tests for titletranslate.py
+    """
+    def test_translate(self):
+        
