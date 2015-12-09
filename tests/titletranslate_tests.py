8d597a39956d5197af77dcfd6474f49d71d5e7cf
Kai Aldag <kaialdag@Kais-MacBook-Pro.local>
2015-12-08 23:45:35 -0800

    Added minimal tests to titletranslate

--- tests/titletranslate_tests.py
+++ tests/titletranslate_tests.py
@@ -6,12 +6,27 @@
 # Distributed under the terms of the MIT license.
 #
 
-from piwikibot import translate
+import pywikibot.page
+import pywikibot.site
+import pywikibot.titletranslate
 
 import unittest
 
 class TestTitleTranslate(unittest.TestCase):
     """ Tests for titletranslate.py
     """
+
     def test_translate(self):
-        
+        print("")
+        try:
+            site = pywikibot.Site()
+            page = pywikibot.Page(site, u"Wikimedia_Foundation")
+        except Exception as error:
+            print("fail to load " + page.title)
+        self.assertEqual(pywikibot.titletranslate.translate(page=page), [])
+
+if __name__ == '__main__':
+    try:
+        unittest.main()
+    except SystemExit:
+        pass
